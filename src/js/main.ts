76d9d894827db003b287a9d1b24885fcf7542eac
Tim <tim@londschien.com>
2022-09-08 06:59:58 +0200

    better bounce and loading

--- src/js/main.ts
+++ src/js/main.ts
@@ -7,12 +7,27 @@ export const setHouseScore = ({ name, points }: House) => {
     document.querySelector<HTMLElement>(`#${name} .points`)!.innerText = points.toString();
     currentPoints[name] = points;
     realPoints[name] = points;
+    updateLS();
 }
 
 export const updateHouseScore = ({ name, points }: House) => {
     realPoints[name] = points;
     transitionPoints();
+    updateLS();
 }
+
+const updateLS = () => {
+    localStorage.setItem("points", JSON.stringify(realPoints));
+};
+
+const loadLS = () => {
+    const points = localStorage.getItem("points");
+    if (points) {
+        const parsedPoints = JSON.parse(points) as Record<House["name"], number>;
+        Object.entries(parsedPoints).forEach(([name, points]) => setHouseScore({ name: name as House["name"], points }));
+    }
+};
+loadLS();
 
 let inTransistion = false;
 const transitionPoints = () => {
@@ -22,14 +37,19 @@ const transitionPoints = () => {
     requestAnimationFrame(() => {
         let somethingChanged = false;
         Object.entries(currentPoints)
-            .filter(([name, points]) => points !== realPoints[name])
+            .filter(([name, points]) => {
+                if (points !== realPoints[name]) return true;
+                document.querySelector<HTMLElement>(`#${name}`)?.classList.remove("bouncing");
+                return false;
+            })
             .forEach(([name, points]) => {
                 currentPoints[name] = points + Math.sign(realPoints[name] - points);
+                document.querySelector<HTMLElement>(`#${name}`)?.classList.add("bouncing");
                 document.querySelector<HTMLElement>(`#${name} .imgContainer`)!.animate(bounce, timeing);
                 document.querySelector<HTMLElement>(`#${name} .points`)!.animate(bounce, timeing);
+                // document.querySelector<HTMLElement>(`#${name}`)!.animate(flash, { ...timeing, easing: "ease-out" });
                 setTimeout(() => document.querySelector<HTMLElement>(`#${name} .points`)!.innerText = currentPoints[name].toString(), timeing.duration / 2);
                 somethingChanged = true;
-                console.log("bounnce");
             });
 
         if (!somethingChanged) return inTransistion = false;
@@ -51,6 +71,16 @@ const bounce = [
     }
 ]
 
+const flash = [{
+        filter: "brightness(1)",
+    }, {
+        filter: "brightness(1.1)",
+    }, {
+        filter: "brightness(1)",
+        offset: 0.7,
+    }
+];
+
 const timeing = {
     duration: 250,
     easing: 'ease-in-out',
@@ -68,7 +98,6 @@ document.querySelectorAll<HTMLElement>('.house').forEach(house => {
             .forEach(house => house.classList.toggle('selected'));
     });
 
-    console.log(house.querySelector<HTMLInputElement>('input'));
     house.querySelector<HTMLInputElement>('form')!.addEventListener('submit', async (e) => {
         const input = (e.target as HTMLElement).querySelector<HTMLInputElement>('input')!;
         const code = input.value;
@@ -77,15 +106,16 @@ document.querySelectorAll<HTMLElement>('.house').forEach(house => {
         e.preventDefault();
 
 
+        const error = house.querySelector<HTMLElement>('.error')!;
 
         const res = await applyCode(code, house.id);
         if (typeof res === "number") {
-            // updateHouseScore({ name: house.id as House["name"], points: realPoints[house.id as House["name"]] + res });
+            updateHouseScore({ name: house.id as House["name"], points: realPoints[house.id as House["name"]] + res });
             house.classList.remove('selected');
+            error.innerText = '';
             return
         }
 
-        const error = house.querySelector<HTMLElement>('.error')!;
         error.innerText = res;
     });
 });
