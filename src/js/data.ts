094a91ea11fb23662ddb2c68a6d5ee2b4d717244
Tim <tim@londschien.com>
2022-09-08 02:49:01 +0200

    flip, code input

--- src/js/data.ts
+++ src/js/data.ts
@@ -1,20 +1,50 @@
 import { createClient } from '@supabase/supabase-js'
-import { updateHouseScore } from './main';
+import { realPoints, setHouseScore, updateHouseScore } from './main';
 
 const supabase = createClient(
   'https://njpqrbzzwregemipdesj.supabase.co',
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5qcHFyYnp6d3JlZ2VtaXBkZXNqIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjI1NjYyMzksImV4cCI6MTk3ODE0MjIzOX0.TvtUh1FKXzueDKsVoEghH5TX98TAm0zqaBMdDIkJ6b8'
 );
 
-export type House = { name: string, points: number };
+export type House = { name: "Gryffindor" | "Slytherin" | "Ravenclaw" | "Hufflepuff", points: number };
+export type Code = { code: string, points: number, isUsed: boolean };
 
 const { data } = await supabase
     .from<House>('house')
     .select('*');
 
-data!.forEach(updateHouseScore);
+data!.forEach(setHouseScore);
 
 supabase
     .from<House>('house')
     .on('*', payload => updateHouseScore(payload.new))
     .subscribe();
+
+const verifyCode = (code: string) => supabase
+    .from<Code>("code")
+    .select("*")
+    .match({ code });
+
+export const applyCode = async (code: string, house: string) => {
+    code = code.toUpperCase();
+    const { data: codes } = await verifyCode(code);
+    console.log(codes);
+
+    if (codes!.length === 0) return "Invalid code";
+    const { isUsed, points } = codes![0];
+
+    console.log(isUsed, points);
+    if (isUsed) return "Code already used";
+
+    await supabase
+        .from("code")
+        .update({ isUsed: true })
+        .match({ code });
+
+    await supabase
+        .from("house")
+        .update({ points: realPoints[house] + points })
+        .match({ name: house });
+
+    return points;
+}
--- src/js/main.ts
+++ src/js/main.ts
@@ -1,3 +1,91 @@
-import { House } from "./data";
+import { House, applyCode } from "./data";
 
-export const updateHouseScore = ({ name, points }: House) => document.querySelector<HTMLElement>(`#${name} > .points`)!.innerText = points.toString();
+const currentPoints = {} as Record<House["name"], number>;
+export const realPoints = {} as Record<House["name"], number>;
+
+export const setHouseScore = ({ name, points }: House) => {
+    document.querySelector<HTMLElement>(`#${name} .points`)!.innerText = points.toString();
+    currentPoints[name] = points;
+    realPoints[name] = points;
+}
+
+export const updateHouseScore = ({ name, points }: House) => {
+    realPoints[name] = points;
+    transitionPoints();
+}
+
+let inTransistion = false;
+const transitionPoints = () => {
+    if (inTransistion) return;
+    inTransistion = true;
+
+    requestAnimationFrame(() => {
+        let somethingChanged = false;
+        Object.entries(currentPoints)
+            .filter(([name, points]) => points !== realPoints[name])
+            .forEach(([name, points]) => {
+                currentPoints[name] = points + Math.sign(realPoints[name] - points);
+                document.querySelector<HTMLElement>(`#${name} .imgContainer`)!.animate(bounce, timeing);
+                document.querySelector<HTMLElement>(`#${name} .points`)!.animate(bounce, timeing);
+                setTimeout(() => document.querySelector<HTMLElement>(`#${name} .points`)!.innerText = currentPoints[name].toString(), timeing.duration / 2);
+                somethingChanged = true;
+                console.log("bounnce");
+            });
+
+        if (!somethingChanged) return inTransistion = false;
+
+        setTimeout(() => {
+            inTransistion = false;
+            transitionPoints();
+        }, timeing.duration);
+    });
+};
+
+const bounce = [
+    {
+        transform: 'translateY(0px)',
+    }, {
+        transform: 'translateY(-1rem)',
+    }, {
+        transform: 'translateY(0px)',
+    }
+]
+
+const timeing = {
+    duration: 250,
+    easing: 'ease-in-out',
+};
+
+
+
+document.querySelectorAll<HTMLElement>('.house').forEach(house => {
+    house.addEventListener('click', (e) => {
+        if ((e.target as HTMLElement).tagName === 'INPUT') return;
+
+        house.classList.toggle('selected');
+        Array(...document.querySelectorAll<HTMLElement>('.selected'))
+            .filter(el => el !== house)
+            .forEach(house => house.classList.toggle('selected'));
+    });
+
+    console.log(house.querySelector<HTMLInputElement>('input'));
+    house.querySelector<HTMLInputElement>('form')!.addEventListener('submit', async (e) => {
+        const input = (e.target as HTMLElement).querySelector<HTMLInputElement>('input')!;
+        const code = input.value;
+        input.value = '';
+
+        e.preventDefault();
+
+
+
+        const res = await applyCode(code, house.id);
+        if (typeof res === "number") {
+            // updateHouseScore({ name: house.id as House["name"], points: realPoints[house.id as House["name"]] + res });
+            house.classList.remove('selected');
+            return
+        }
+
+        const error = house.querySelector<HTMLElement>('.error')!;
+        error.innerText = res;
+    });
+});
